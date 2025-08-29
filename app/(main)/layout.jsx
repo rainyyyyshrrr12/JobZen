fad7d353e91ec798223ccc1b2162bb2a50b96398
Rainy Sharma <raiinysharmaa.20@gmail.com>
2025-08-29 09:10:57 +0530

    basic structure

---
+++ app/(main)/layout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ClerkProvider } from '@clerk/nextjs';
+ // Import your global CSS
+
+export const metadata = {
+  title: "Sensai - AI Career Assistant",
+  description: "Your AI-powered career development platform",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <div className="container mx-auto mt-24 mb-20">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
